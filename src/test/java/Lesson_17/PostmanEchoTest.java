3fe14e60bc688fced1ce2708894d685d25e67ad2
anastalinkevich <anastalinkevich@gmail.com>
2024-12-23 20:15:35 +0300

    Задание по лекции 17. Коммит 2

--- src/test/java/Lesson_17/PostmanEchoTest.java
+++ src/test/java/Lesson_17/PostmanEchoTest.java
@@ -1,6 +1,7 @@
 package Lesson_17;
 
 import io.restassured.http.ContentType;
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -14,24 +15,24 @@ public class PostmanEchoTest {
     @Test
     @DisplayName("Метод GET")
     public void givenStatus(){
-
         given().log().all()
-
-        .when().get("https://postman-echo.com/get?foo1=bar1&foo2=bar2")
-        .then().log().body().statusCode(200)
-                .and().body("args.foo1", equalTo("bar1"))
-                .and().body("args.foo2", equalTo("bar2"))
-                .and().body("headers.host", equalTo("postman-echo.com"))
-                .and().body("headers.x-request-start", equalTo("t1734966710.725"))
-                .and().body("headers.connection", equalTo("close"))
-                .and().body("headers.x-forwarded-proto", equalTo("https"))
-                .and().body("headers.x-forwarded-port", equalTo("443"))
-                .and().body("headers.x-amzn-trace-id", equalTo("Root=1-67697db6-73caa1c20f2ca2546c410d15"))
-                .and().body("headers.user-agent", equalTo("PostmanRuntime/7.43.0"))
-                .and().body("headers.accept", equalTo("*/*"))
-                .and().body("headers.cache-control", equalTo("no-cache"))
+                .when().get("https://postman-echo.com/get?foo1=bar1&foo2=bar2")
+                .then().log().body()
+                .statusCode(200)
+                .and().body("args.foo1", Matchers.is("bar1"))
+                .and().body("args.foo2", Matchers.is("bar2"))
+                .and().body("headers.host", Matchers.is("postman-echo.com"))
+                .and().body("headers.x-request-start", notNullValue())
+                .and().body("headers.connection", Matchers.is("close"))
+                .and().body("headers.x-forwarded-proto", Matchers.is("https"))
+                .and().body("headers.x-forwarded-port", Matchers.is("443"))
+                .and().body("headers.x-amzn-trace-id", notNullValue())
+                .and().body("headers.user-agent", Matchers.is("PostmanRuntime/7.43.0"))
+                .and().body("headers.accept", Matchers.is("*/*"))
+                .and().body("headers.cache-control", Matchers.is("no-cache"))
                 .and().body("headers.postman-token", notNullValue())
-                .and().body("headers.x-request-start", equalTo("postman-token"))
-                .and().body("url", equalTo("https://postman-echo.com/get?foo1=bar1&foo2=bar2"));
+                .and().body("headers.accept-encoding", Matchers.is("gzip, deflate, br"))
+                .and().body("headers.cookie", notNullValue() )
+                .and().body("url", Matchers.is("https://postman-echo.com/get?foo1=bar1&foo2=bar2"));
     }
 }
